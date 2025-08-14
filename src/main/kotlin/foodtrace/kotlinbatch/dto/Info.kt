7de840c3321a68566ac1e3a66f4a28d78deeb1e6
sunjaewow <chltjswo789@naver.com>
2025-08-14 20:36:24 +0900

    feat: create batch

---
+++ src/main/kotlin/foodtrace/kotlinbatch/dto/Info.kt
@@ -0,0 +1,7 @@
+package foodtrace.kotlinbatch.dto
+
+data class Info(
+    val id : Long,
+    val name : String,
+    val price : Long
+)
--- src/main/kotlin/foodtrace/kotlinbatch/service/FetchService.kt
+++ src/main/kotlin/foodtrace/kotlinbatch/service/FetchService.kt
@@ -2,4 +2,5 @@ package foodtrace.kotlinbatch.service
 
 interface FetchService {
     fun fetchAllAndSave(key : Int)
+    fun batch() : List<Info>
 }
